dd9b1a495db23b3734930267dbc959652433bc83
riyadi rb <riyadi.rb@gmail.com>
2022-11-25 09:05:07 +0700

    no message

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const baseUrl = "https://openapi.majalengkadev.com/api/";
-const apiToken = "";
+const apiToken = "iji2vo52zxFaLbke5TCJsSTKd8LnrdGCWYSc5eYo";
 
 const backgroundColor = Color(0xFFf4f5f4);
 const iconColor = Color(0xffb1b1b2);
