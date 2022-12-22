39a194d4a8939cf7dc37bdcaba730e59f2ec3550
riyadi rb <riyadi.rb@gmail.com>
2022-12-22 19:29:46 +0700

    - change new api-key

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const baseUrl = "https://openapi.majalengkadev.com/api/";
-const apiToken = "lramQUm3WdxMV1YxvSFRsB1k8BzuJOLPBPFkZwDI";
+const apiToken = "0sKsihs57VrOltrQRmBHcw4l3AfRrVeZoOzkFpmh";
 
 const backgroundColor = Color(0xFFf4f5f4);
 const iconColor = Color(0xffb1b1b2);
